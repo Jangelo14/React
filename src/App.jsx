c059b07a833417b534908d2942231fdb389a6bbd
Jangelo14 <joao.fogaca.ti@gmail.com>
2024-06-11 21:31:30 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Aveztruz</h1>
+        <img src="https://www.google.com/imgres?q=avestruz&imgurl=https%3A%2F%2Fboxloja-std-assets.nyc3.cdn.digitaloceanspaces.com%2Flojas%2Fq2r07%2Fprodutos%2Fe50601f6-e0fc-4e76-a0d4-9f9ef50c4d53.jpg&imgrefurl=https%3A%2F%2Fwww.mercadordeantiguidades.com.br%2F20a808%2Ftaxidermia-filhote-de-avestruz-em-ovo-origem-africana-medindo-17cm&docid=MRgFQ1cjWvk4gM&tbnid=RM5m6Qs-7eTF_M&vet=12ahUKEwjTi9yh59SGAxWSpJUCHX5ICCIQM3oECH0QAA..i&w=904&h=960&hcb=2&ved=2ahUKEwjTi9yh59SGAxWSpJUCHX5ICCIQM3oECH0QAA" alt="Avestruz" width="250"/>
+        <p>Criaçao de avestruz no interior do sertao</p>
+
+        <button onclick={() => alert ('Saiba mais sobre os Avestruz')}Saiba mais></button>
+      </div>
+    </>
+  )
+}
+
+export default App
